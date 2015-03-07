db700246428fc4a8f51a87fd87ed2dee4a0abb8e
cloverrose <clover.plus.rose@gmail.com>
2015-03-07 19:58:57 +0900

    uncomment

--- flaskr.py
+++ flaskr.py
@@ -25,31 +25,31 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 @app.route('/api/v0/alps', methods=['POST'])
 def receive_alps():
     val_No = request.form['No']
-    # val_MagX = request.form['MagX']
-    # val_MagY = request.form['MagY']
-    # val_MagZ = request.form['MagZ']
-    # val_AccX = request.form['AccX']
-    # val_AccY = request.form['AccY']
-    # val_AccZ = request.form['AccZ']
-    # val_Uv = request.form['Uv']
-    # val_Lx = request.form['Lx']
-    # val_Humi = request.form['Humi']
-    # val_Temp = request.form['Temp']
-    # val_Press = request.form['Press']
+    val_MagX = request.form['MagX']
+    val_MagY = request.form['MagY']
+    val_MagZ = request.form['MagZ']
+    val_AccX = request.form['AccX']
+    val_AccY = request.form['AccY']
+    val_AccZ = request.form['AccZ']
+    val_Uv = request.form['Uv']
+    val_Lx = request.form['Lx']
+    val_Humi = request.form['Humi']
+    val_Temp = request.form['Temp']
+    val_Press = request.form['Press']
 
     return render_template('index.html',
-                           No=val_No)
-                           # MagX=val_MagX,
-                           # MagY=val_MagY,
-                           # MagZ=val_MagX,
-                           # AccX=val_AccX,
-                           # AccY=val_AccY,
-                           # AccZ=val_AccZ,
-                           # Uv=val_Uv,
-                           # Lx=val_Lx,
-                           # Humi=val_Humi,
-                           # Temp=val_Temp,
-                           # Press=val_Temp)
+                           No=val_No,
+                           MagX=val_MagX,
+                           MagY=val_MagY,
+                           MagZ=val_MagX,
+                           AccX=val_AccX,
+                           AccY=val_AccY,
+                           AccZ=val_AccZ,
+                           Uv=val_Uv,
+                           Lx=val_Lx,
+                           Humi=val_Humi,
+                           Temp=val_Temp,
+                           Press=val_Temp)
                 
 
 if __name__ == '__main__':
